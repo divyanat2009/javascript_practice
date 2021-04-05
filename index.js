4c65f00e93ebd23bb06956b36a984cee12ea7903
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-04-04 22:37:40 -0500

    check duplicate

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function reverseString(str){
   return str.split("").reverse().join("")
 }
 reverseString("hello");
+
+//Check Duplicate
+
+function checkDuplicate(){
+  let arr = ["aa", "bb", "aa"];
+  let result = false;
+  //iterate over the array
+  for(let i = 0; i < arr.length; i++){
+    //compare the first and last index
+    if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])){
+      result = true;
+      break;
+    }
+  }
+  if(result){
+    console.log("Array contains duplicates");
+  }else{
+    console.log("Array does not contain duplicates")
+  }
+}
+checkDuplicate();
